369d874d18362fc2e43ad53e8afbdf1dc95987cd
Deepan Das <ddas@scortex.io>
2020-03-13 04:04:55 -0500

    Training Scripts

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import math
 import argparse
 import shutil
 
-from dataloader import trainloader
-from models import EncoderCNN, DecoderRNN
+from data.dataloader import DataLoader
+from models.models import EncoderCNN, DecoderRNN
+from utils import utils
+import train
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -15,16 +17,23 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--lr', default=0.001, type=float)
 parser.add_argument('--n_epochs', default=10)
 parser.add_argument('-b', default=1)
-parser.add_argument('--gpu', deafult=False)
+parser.add_argument('--gpu', default=False)
+parser.add_argument('--name', default="DefaultRun")
+parser.add_argument('--resume', default='')
 
-
+_DATASET_PATH = '../data/Data/DataSet13_20200221/raw_patient_based'
 def main(args):
 
     print("Process %s, running on %s: starting (%s)" % (
         os.getpid(), os.name, time.asctime()))
 
+    use_gpu = args.gpu
+    if not torch.cuda.is_available():
+        use_gpu = False
+
     encoder = EncoderCNN()
     decoder = DecoderRNN()
+
     if torch.cuda.is_available() and args.gpu:
         encoder = encoder.cuda()
         decoder = decoder.cuda()
@@ -40,17 +49,51 @@ def main(args):
         transforms.Normalize((0.485, 0.456, 0.406),
                              (0.229, 0.224, 0.225))])
 
-    data_loader = trainloader(transform=transform)
+    train_loader = DataLoader(transform=transform, mode="train", dataset_path=_DATASET_PATH)
+    val_loader = DataLoader(transform=transform, mode="val", dataset_path=_DATASET_PATH)
+
+    start_epoch, best_loss = utils.load_checkpoint(encoder, decoder, args.resume)
+
     optimizer = torch.optim.SGD(params=params, lr=args.lr, momentum=0.9)
 
-   train_steps = math.ceil()
-   epoch = 0
+    # Batch Size = 1 in this case
+    train_steps = math.ceil(len(train_loader))
+    epoch = start_epoch
+    best_epoch = start_epoch
 
-   while epoch <= args.n_epochs:
-       # Create training script in train.py
-       # Create validation script
-       # Create testing script
-        pass
+    while epoch <= int(args.n_epochs):
+        print("========================================================")
+
+        print("Epoch %d Training Starting" % epoch)
+        print("Learning Rate : ", utils.get_lr(optimizer))
+
+        train_loss = train.train(train_loader, encoder, decoder,
+                           optimizer, epoch, use_gpu)
+        val_loss = train.validate(encoder, decoder, val_loader, epoch, use_gpu)
+
+        print("Training Loss: ", float(train_loss.data))
+        print("Validation Loss: ", float(val_loss.data))
+        print("========================================================")
+
+        utils.save_checkpoint(
+            {
+                "epoch": epoch,
+                "best_loss": min(best_loss, val_loss),
+                "encoder": encoder.state_dict(),
+                "decoder": decoder.state_dict()
+            },
+            val_loss < best_loss,
+            best_epoch=epoch,
+            best_loss=val_loss,
+            dir_name=args.name
+        )
+        if val_loss < best_loss:
+          best_epoch = epoch
+          best_loss = val_loss
+
+        epoch += 1
 
 
-   pass
+if __name__ == "__main__":
+  args = parser.parse_args()
+  main(args)
--- train.py
+++ train.py
@@ -1,3 +1,110 @@
+import time
 import torch
 import numpy as np
 import math
+from statistics import mean
+import torch.utils.data as data
+from tensorboardX import SummaryWriter
+from torch.nn import MSELoss
+
+writer = SummaryWriter('../logs')
+
+from utils.utils import AverageMeter
+
+
+_mse_loss = torch.nn.MSELoss()
+
+def train(data_loader_train, encoder, decoder, optimizer, epoch,
+          use_gpu=False, start_loss=0.0):
+
+    losses = AverageMeter()
+    total_loss = start_loss
+
+    start_time = time.time()
+
+    # Randomly Shuffled Dataset Indices
+    indices = data_loader_train.get_indices()
+    total_steps = len(indices)
+
+    # Loss Scores Record
+    loss_scores = list()
+
+    # Training with Batch Size 1
+    for index in range(total_steps):
+        # Fetch actual data index
+        d_index = indices[index]
+
+        encoder.train()
+        decoder.train()
+
+        img_sequence, label = data_loader_train[d_index]
+
+        if torch.cuda.is_available() and use_gpu:
+            img_sequence = img_sequence.cuda()
+            label = label.cuda()
+
+        seq_op = encoder(img_sequence)
+        pred = decoder(seq_op)
+        loss = _mse_loss(pred, label)
+
+        optimizer.zero_grad()
+        total_loss += loss.data
+        loss.backward()
+        optimizer.step()
+        # print("000000", loss, loss.shape)
+        losses.update(loss.item())
+        niter = epoch * total_steps + index
+        writer.add_scalar('data/training_loss', losses.val, niter)
+
+        print("Step: %d, Current Loss: %0.4f, Average Loss: %0.4f" %
+              (index, loss, total_loss))
+
+    time_taken = time.time() - start_time
+
+    return total_loss/total_steps
+
+
+
+def validate(encoder, decoder, data_loader_val, epoch, use_gpu):
+
+    val_losses = AverageMeter()
+    total_val_loss = 0.0
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    encoder = encoder.to(device)
+    decoder = decoder.to(device)
+    encoder.eval()
+    decoder.eval()
+
+    indices = data_loader_val.get_indices()
+    total_steps = len(indices)
+
+    # Loss Scores Record
+    loss_scores = list()
+    for index in range(total_steps):
+        d_index = indices[index]
+
+        img_sequence, label = data_loader_train[d_index]
+
+        img_sequence = img_sequence.to(device)
+        label = label.to(deice)
+        
+        with torch.no_grad():
+            seq_op = encoder(img_sequence)
+            pred = decoder(seq_op)
+
+            loss = _mse_loss(pred, label)
+            loss_scores.append(loss)
+            total_val_loss += loss.data[0]
+
+        val_losses.update(loss.item())
+        niter =  epoch * total_steps + index
+        writer.add_scalar('data/validation_loss', val_losses.val, niter)
+
+    return total_val_loss / total_steps
+
+
+
+
+
